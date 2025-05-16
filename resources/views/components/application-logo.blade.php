5d39970238e9e9c789e6c370bef7dffbb66ab918
TomasCh1 <xchmelan@stuba.sk>
2025-05-16 14:43:23 +0200

    Initial commit

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,4 @@
+<!-- resources/views/components/application-logo.blade.php -->
+<img src="https://th.bing.com/th/id/R.2b2b1304d99e55815005cc8a0b97907a?rik=59z9Zpg7gsft1w&riu=http%3a%2f%2fwww.ktl.elf.stuba.sk%2fprojects%2fngn%2fimages%2flogo-fei.jpg&ehk=6by%2boMuqueYFntz3e8mIbrBLF9QuHy3xJxEM3S6%2fGPg%3d&risl=&pid=ImgRaw&r=0"
+     alt="Logo FEI STU"
+        {{ $attributes }}>
