9cd8f4b1f80739c218ccf5ee31a7817404e8f5e2
Marek Matkovic <xmatkovic@stuba.sk>
2025-05-21 22:46:49 +0200

    Guest dashboard z loginu

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -60,7 +60,11 @@
 
                 <div class="flex items-center justify-end">
                     <x-primary-button>Prihlásiť</x-primary-button>
+
                 </div>
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('dashboard') }}">
+                    {{ __('Pokračovať ako hosť') }}
+                </a>
             </form>
         </div>
 
